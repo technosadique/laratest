bd472b737635e7270b2fabe6f88fd2851fb3ce32
sadique mohammad <sadik.jsr@gmail.com>
2025-07-31 19:12:43 +0530

    code updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     function index(Request $req){ 
-		$blogs=[['name'=>'Hello World'],['name'=>'My Country']];
+		$blogs=[['name'=>'The Rise of Generative AI'],['name'=>'10 Ways AI Can Make You More Productive at Work'],['name'=>'Top AI Breakthroughs That Are Defining the Decade'],['name'=>'The Future Is Now: How AI Is Changing Everyday Life'],['name'=>'AI in 2025: What to Expect from the Next Wave of Innovation']];
 		
 		//print_r($blogs);die;
 		
